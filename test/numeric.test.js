8d5b225db23a08a80ecfb633c218d218a52a35c1
Mati <matiboux@gmail.com>
2024-08-13 00:11:10 +0200

    Add tests to improve coverage (#6)
    * Test forced redundancy
    
    * Test the version getter
    
    * Test the character sets getter
    
    * Test the default parameters getter
    
    * Test the last parameters getter
    
    * Add COVERAGE_LCOV env var
    
    * Report lcov depending on config
    
    * Add numeric keygen tests
    
    * Test numeric keygen with no redundancy

---
+++ test/numeric.test.js
@@ -0,0 +1,48 @@
+const KeyGenLib = require('../KeyGenLib')
+
+test('generates a numeric keygen', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		const parameters = {
+			numeric: true,
+			lowercase: false,
+			uppercase: false,
+			special: false,
+			length: 12,
+			redundancy: true,
+		}
+		KeyGen.setParameters(parameters)
+		expect(KeyGen.generateKeygen()).toBeTruthy()
+	})
+
+test('generates a numeric keygen with forced redundancy', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		const parameters = {
+			numeric: true,
+			lowercase: false,
+			uppercase: false,
+			special: false,
+			length: 12,
+			redundancy: false,
+		}
+		KeyGen.setParameters(parameters)
+		expect(KeyGen.generateKeygen()).toBeTruthy()
+		expect(KeyGen.parameters.redundancy).toBe(true)
+	})
+
+test('generates a numeric keygen with no redundancy', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		const parameters = {
+			numeric: true,
+			lowercase: false,
+			uppercase: false,
+			special: false,
+			length: 10,
+			redundancy: false,
+		}
+		KeyGen.setParameters(parameters)
+		expect(KeyGen.generateKeygen()).toBeTruthy()
+		expect(KeyGen.parameters.redundancy).toBe(false)
+	})
---
+++ test/static.test.js
@@ -0,0 +1,47 @@
+const KeyGenLib = require('../KeyGenLib')
+
+test('gets the version', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		expect(KeyGen.version).toMatch(/^\d+\.\d+\.\d+$/)
+	})
+
+test('gets the character sets', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		expect(KeyGen.characterSets).toBeDefined()
+		expect(KeyGen.characterSets.numeric).toMatch(/^[0-9]+$/)
+		expect(KeyGen.characterSets.lowercase).toMatch(/^[a-z]+$/)
+		expect(KeyGen.characterSets.uppercase).toMatch(/^[A-Z]+$/)
+		expect(KeyGen.characterSets.special).toMatch(/^[^a-zA-Z0-9]+$/)
+	})
+
+test('gets the default parameters', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		expect(KeyGen.defaultParameters).toBeDefined()
+		expect(KeyGen.defaultParameters.numeric).toBeDefined()
+		expect(KeyGen.defaultParameters.lowercase).toBeDefined()
+		expect(KeyGen.defaultParameters.uppercase).toBeDefined()
+		expect(KeyGen.defaultParameters.special).toBeDefined()
+		expect(KeyGen.defaultParameters.length).toBeGreaterThan(0)
+		expect(KeyGen.defaultParameters.redundancy).toBeDefined()
+	})
+
+test('gets the last parameters', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		expect(KeyGen.lastParameters).toBeDefined()
+		expect(KeyGen.lastParameters.numeric).toBeDefined()
+		expect(KeyGen.lastParameters.lowercase).toBeDefined()
+		expect(KeyGen.lastParameters.uppercase).toBeDefined()
+		expect(KeyGen.lastParameters.special).toBeDefined()
+		expect(KeyGen.lastParameters.length).toBeGreaterThan(0)
+		expect(KeyGen.lastParameters.redundancy).toBeDefined()
+	})
+
+test('asserts last parameters are default parameters', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		expect(KeyGen.lastParameters).toEqual(KeyGen.defaultParameters)
+	})
