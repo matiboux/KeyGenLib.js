3386f47a1511a1661458e3854f9a72676d19f7dd
Matiboux <matiboux@gmail.com>
2024-08-12 23:10:08 +0200

    Test forced redundancy

--- test/long-special.test.js
+++ test/long-special.test.js
@@ -10,3 +10,19 @@ test('generates a long password with special characters', () =>
 		KeyGen.setParameters(parameters)
 		expect(KeyGen.generateKeygen()).toBeTruthy()
 	})
+
+test('generates a long password with forced redundancy', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		const parameters = {
+			numeric: true,
+			lowercase: false,
+			uppercase: false,
+			special: false,
+			length: 64,
+			redundancy: false,
+		}
+		KeyGen.setParameters(parameters)
+		expect(KeyGen.generateKeygen()).toBeTruthy()
+		expect(KeyGen.parameters.redundancy).toBe(true)
+	})
