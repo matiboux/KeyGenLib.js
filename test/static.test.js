0f6d8a697c54b50ed1d21f2a2cd339f1859a3495
Matiboux <matiboux@gmail.com>
2024-08-12 23:18:42 +0200

    Test the character sets getter

--- test/static.test.js
+++ test/static.test.js
@@ -5,3 +5,13 @@ test('gets the version', () =>
 		const KeyGen = new KeyGenLib()
 		expect(KeyGen.version).toMatch(/^\d+\.\d+\.\d+$/)
 	})
+
+test('gets the character sets', () =>
+	{
+		const KeyGen = new KeyGenLib()
+		expect(KeyGen.characterSets).toBeDefined()
+		expect(KeyGen.characterSets.numeric).toMatch(/^[0-9]+$/)
+		expect(KeyGen.characterSets.lowercase).toMatch(/^[a-z]+$/)
+		expect(KeyGen.characterSets.uppercase).toMatch(/^[A-Z]+$/)
+		expect(KeyGen.characterSets.special).toMatch(/^[^a-zA-Z0-9]+$/)
+	})
